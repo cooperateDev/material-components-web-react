ff7222836886cbb0a18261ab0fed78c2a7b90b08
cooperateDev <pavelasanov1206@gmail.com>
2018-03-30 15:25:20 -0700

    WIP: test screenshots

--- test/screenshot/full-suite.js
+++ test/screenshot/full-suite.js
@@ -1,7 +1,7 @@
-import temporaryPackageSuite from './temporary-package/screenshot-suite';
+import topAppBarSuite from './top-app-bar/screenshot-suite';
 
 const fullSuite = [
-  temporaryPackageSuite,
+  topAppBarSuite,
 ];
 
 export default fullSuite;
---
+++ test/screenshot/top-app-bar/standard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TopAppBar from '../../../packages/top-app-bar';
+import LoremIpsumContent from './LoremIpsumContent';
+import NavIcon from './NavIcon';
+import actionItems from './actionItems';
+
+import '../../../packages/top-app-bar/index.scss';
+import './index.scss';
+
+ReactDOM.render((
+  <div>
+    <TopAppBar
+      title='Miami, Fl'
+      navIcon={<NavIcon />}
+      actionItems={actionItems}
+    />
+
+    <LoremIpsumContent />
+  </div>
+), document.getElementById('app'));
