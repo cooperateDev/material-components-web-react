a428f7ddbe33c0e9477632ce0ee7a87b58b20349
cooperateDev <pavelasanov1206@gmail.com>
2018-04-09 16:00:50 -0700

    chore: Use `includePaths` instead of `importer` in `sass-loader` config (#15)

--- test/screenshot/webpack.config.js
+++ test/screenshot/webpack.config.js
@@ -1,9 +1,7 @@
 const materialIconBundles = require('./material-icon/webpack.config.js');
 const topAppBarBundles = require('./top-app-bar/webpack.config.js');
-const fabBundles = require('./fab/webpack.config.js');
 
 module.exports = [
   ...materialIconBundles,
   ...topAppBarBundles,
-  ...fabBundles,
 ];
